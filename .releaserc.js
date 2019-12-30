864c172aa2037b586b9373d672a94a6c4346b665
Florian Forster <florian@caos.ch>
2019-12-30 16:20:59 +0100

    fix: try it with a workaround

--- .releaserc.js
+++ .releaserc.js
@@ -4,5 +4,8 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/github",
+        ["@semantic-release/exec", {
+            "publishCmd": "echo ::set-env name=VERSION::v${nextRelease.version}"
+          }],
     ]
   };
