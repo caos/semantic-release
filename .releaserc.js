f061a9bd44625c34265cc399af546bc6d2d8ac14
Florian Forster <florian@caos.ch>
2019-12-30 16:33:32 +0100

    build: remove exec

--- .releaserc.js
+++ .releaserc.js
@@ -3,9 +3,6 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
-        ["@semantic-release/exec", {
-            "publishCmd": "echo ::set-env name=VERSION::v${nextRelease.version}"
-          }],
+        "@semantic-release/github"
     ]
   };
